a32e310a42e9d12bb3420d4bc4c6373034cbc765
root <root@debian>
2015-11-12 10:22:54 +0200

    removed two svg's from vendor/gnome which were issuing errors in mogrify command

--- resources/main.ts
+++ resources/main.ts
@@ -1,4 +1,4 @@
-/// <reference path="C:\\Users\\andrei\\Desktop\\Constraint\\js\\main.ts" />
+/// <reference path="/srv/www/websites/adds.admin.rcs-rds.ro/htdocs/vendor/Constraint/js/main.ts" />
 
 
 UI_Resource.addResourceFile( "Constraint", "resources/Constraint.res" );
